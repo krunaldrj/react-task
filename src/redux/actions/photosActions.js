9ada3e40173861d983a4580e34ddee821bfaa18a
Nikesh <nileshvishwkarma09@gmail.com>
2021-09-18 15:34:28 +0530

    commit

--- src/redux/actions/photosActions.js
+++ src/redux/actions/photosActions.js
@@ -3,17 +3,11 @@ import axios from 'axios'
 
 export const getPhotos = () => async dispatch => {
     
-    try{
+    
         const res = await axios.get(`http://jsonplaceholder.typicode.com/photos`)
         dispatch( {
             type: GET_PHOTOS,
             payload: res.data
         })
-    }
-    catch(e){
-        dispatch( {
-            payload: console.log(e),
-        })
-    }
 
 }
